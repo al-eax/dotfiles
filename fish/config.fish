67741385eae5d59652730d792508b69e300ea0dd
al-eax <hinze.alex@gmail.com>
2024-02-02 15:31:26 +0100

    wip

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,5 @@
-if status is-interactive
+if status is-interactive 
 and not set -q TMUX
-    if tmux has-session -t home
-        exec tmux attach-session -t home
-    else
-        tmux new-session -s home
-    end
+    exec tmux
 end
-
+clear
