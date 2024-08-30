1d97b9c27ef739d60caf315b032fdaced828f51d
Alex H <hinze.alex@gmail.com>
2024-08-30 13:06:53 +0200

    wip

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
-#if status is-interactive 
-#and not set -q TMUX
-#    exec tmux
-#end
-#clear
+if status is-interactive 
+and not set -q TMUX
+    exec tmux
+end
+clear
 
 set PATH /home/alex/.nvm/versions/node/v20.15.1/bin $PATH
