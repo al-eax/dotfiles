be476082ee6ebe53b2baae09fe3166cab6ac71e8
Alex H <hinze.alex@gmail.com>
2024-09-03 08:08:20 +0200

    wip

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -2,6 +2,14 @@ return {
   "lewis6991/gitsigns.nvim",
   config = function ()
     require('gitsigns').setup({
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+          delay = 1,
+          ignore_whitespace = false,
+          virt_text_priority = 100,
+        },
     })
     vim.api.nvim_create_user_command('Blame', ":Gitsigns toggle_current_line_blame", {}) -- toggle git blame
 
--- nvim/lua/plugins/tabbar.lua
+++ nvim/lua/plugins/tabbar.lua
@@ -1,33 +1,37 @@
 return {
-    "romgrk/barbar.nvim",
-    dependencies = {
-        "lewis6991/gitsigns.nvim",
-        "nvim-tree/nvim-web-devicons"
-    },
-    config = function()
-        require('barbar').setup({
-            maximum_length = 30, -- max file len
-        })
 
-        local map = vim.api.nvim_set_keymap
-        -- Move to previous/next
-        map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', { noremap = true, silent = true, desc = "" })
-        map('n', '<A-.>', '<Cmd>BufferNext<CR>', { noremap = true, silent = true, desc = "" })
-        map('n', '<c-h>', '<Cmd>BufferPrevious<CR>', { noremap = true, silent = true, desc = "" })
-        map('n', '<c-l>', '<Cmd>BufferNext<CR>', { noremap = true, silent = true, desc = "" })
+  'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons',
 
-        -- Goto buffer in position...
-        map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', { noremap = true, silent = true, desc = "" })
-        map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', { noremap = true, silent = true, desc = "" })
-        map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', { noremap = true, silent = true, desc = "" })
-        map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', { noremap = true, silent = true, desc = "" })
-        map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', { noremap = true, silent = true, desc = "" })
-        map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', { noremap = true, silent = true, desc = "" })
-        map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', { noremap = true, silent = true, desc = "" })
-        map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', { noremap = true, silent = true, desc = "" })
-        map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', { noremap = true, silent = true, desc = "" })
-        map('n', '<A-0>', '<Cmd>BufferLast<CR>', { noremap = true, silent = true, desc = "" })
-        vim.keymap.set("n", "<C-q>", "<Cmd>BufferClose!<CR>")
-        vim.keymap.set("n", "<A-q>", "<Cmd>BufferClose!<CR>")
-    end
+
+  config = function()
+
+    vim.opt.termguicolors = true
+
+    require("bufferline").setup{
+      options = {
+        middle_mouse_command = "bdelete! %d", -- close buffer by middle mouse key click
+        diagnostics = "nvim_lsp"}
+    }
+    local map = vim.api.nvim_set_keymap
+    -- Move to previous/next
+    map('n', '<A-,>', '<Cmd>BufferLineCyclePrev<CR>', { noremap = true, silent = true, desc = "" })
+    map('n', '<A-.>', '<Cmd>BufferLineCycleNext<CR>', { noremap = true, silent = true, desc = "" })
+    map('n', '<c-h>', '<Cmd>BufferLineCyclePrev<CR>', { noremap = true, silent = true, desc = "" })
+    map('n', '<c-l>', '<Cmd>BufferLineCycleNext<CR>', { noremap = true, silent = true, desc = "" })
+    map('n', '<a-k>', '<Cmd>BufferLineCycleNext<CR>', { noremap = true, silent = true, desc = "" })
+    map('n', '<a-j>', '<Cmd>BufferLineCyclePrev<CR>', { noremap = true, silent = true, desc = "" })
+
+    -- Goto buffer in position...
+    map('n', '<A-1>', '<Cmd>BufferLineGoToBuffer 1<CR>', { noremap = true, silent = true, desc = "" })
+    map('n', '<A-2>', '<Cmd>BufferLineGoToBuffer 2<CR>', { noremap = true, silent = true, desc = "" })
+    map('n', '<A-3>', '<Cmd>BufferLineGoToBuffer 3<CR>', { noremap = true, silent = true, desc = "" })
+    map('n', '<A-4>', '<Cmd>BufferLineGoToBuffer 4<CR>', { noremap = true, silent = true, desc = "" })
+    map('n', '<A-5>', '<Cmd>BufferLineGoToBuffer 5<CR>', { noremap = true, silent = true, desc = "" })
+    map('n', '<A-6>', '<Cmd>BufferLineGoToBuffer 6<CR>', { noremap = true, silent = true, desc = "" })
+    map('n', '<A-7>', '<Cmd>BufferLineGoToBuffer 7<CR>', { noremap = true, silent = true, desc = "" })
+    map('n', '<A-8>', '<Cmd>BufferLineGoToBuffer 8<CR>', { noremap = true, silent = true, desc = "" })
+    map('n', '<A-9>', '<Cmd>BufferLineGoToBuffer 9<CR>', { noremap = true, silent = true, desc = "" })
+    vim.keymap.set("n", "<C-q>", "<Cmd>:bd<CR>")
+    vim.keymap.set("n", "<A-q>", "<Cmd>:bd<CR>")
+  end
 }
