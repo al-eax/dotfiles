03357f245a392eaaa3a3fe846f7454459a62ed4a
Alex H <hinze.alex@gmail.com>
2024-12-06 09:33:57 +0100

    wip

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,12 @@
-if status is-interactive 
-and not set -q TMUX
-    tmux attach -t home || tmux new -s home
+# Prüfen, ob tmux bereits läuft
+if not set -q TMUX
+    # Startet eine neue tmux-Session mit einem eindeutigen Namen basierend auf der PID
+    echo "NEW TMUX SESSION"
+    set tmux_session_name "session_$fish_pid"
+    tmux new-session -s $tmux_session_name && exit
 end
-clear
 
+
+tmux list-sessions | grep -v attached | cut -d: -f1 |  xargs -t -n1 tmux kill-session -t
 
 set PATH /home/alex/.nvm/versions/node/v20.15.1/bin $PATH
--- nvim/lua/plugins/colortheme.lua
+++ nvim/lua/plugins/colortheme.lua
@@ -3,6 +3,10 @@ vim.api.nvim_create_user_command('Td', ":lua vim.cmd('colorscheme github_dark_co
 vim.api.nvim_create_user_command('Tl', ":lua vim.cmd('colorscheme github_light_colorblind')", {})
 vim.opt.termguicolors = true
 
+vim.cmd([[
+  " set guicursor=n-v-c:block-Cursor,i-ci-ve:ver25-CursorInsert,r-cr:hor20-CursorReplace,o:hor50
+  set guicursor+=a:blinkon1
+]]) -- make cursor visible & make cursor blink
 
 return {
   {
@@ -15,13 +19,15 @@ return {
     'folke/tokyonight.nvim'
   },
   {
-  'projekt0n/github-nvim-theme',
-  name = 'github-theme',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    -- if an error occures, run :GithubThemeCompile
-    vim.cmd('colorscheme github_dark_colorblind')
-  end,
-  }
+    'projekt0n/github-nvim-theme',
+    name = 'github-theme',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- if an error occures, run :GithubThemeCompile
+      vim.cmd('colorscheme github_dark_colorblind')
+      vim.api.nvim_set_hl(0, "CursorLine", { bg = "#444444", underline = false })
+
+    end,
+  },
 }
